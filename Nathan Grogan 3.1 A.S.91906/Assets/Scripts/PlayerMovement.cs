558e72c69028fa038c886e9c64cde73e334edb74
NathanGroganTBC <nathan.grogan@tbc.school.nz>
2023-05-15 13:15:12 +1200

    15/05/23

--- Nathan Grogan 3.1 A.S.91906/Assets/Scripts/PlayerMovement.cs
+++ Nathan Grogan 3.1 A.S.91906/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("RunHorizontal", Input.GetAxis("Horizontal"));
+
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime;
     }
