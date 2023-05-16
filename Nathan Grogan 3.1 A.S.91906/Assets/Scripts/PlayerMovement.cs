ff7249e44a76bb01eb21a7bc76edac5677a759a8
NathanGroganTBC <nathan.grogan@tbc.school.nz>
2023-05-16 14:14:02 +1200

    16/05/23

--- Nathan Grogan 3.1 A.S.91906/Assets/Scripts/PlayerMovement.cs
+++ Nathan Grogan 3.1 A.S.91906/Assets/Scripts/PlayerMovement.cs
@@ -11,8 +11,32 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         animator.SetFloat("RunHorizontal", Input.GetAxis("Horizontal"));
+        animator.SetFloat("RunVertical", Input.GetAxis("Vertical"));
 
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 lookAt = mouseScreenPosition; 
+
+
+        // this code is bad please fix
+
+        float AngleRad = Mathf.Atan2(lookAt.y, lookAt.x);
+
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        
+
         transform.position = transform.position + horizontal * Time.deltaTime;
+        transform.position = transform.position + vertical * Time.deltaTime;
+
+        Quaternion Quater = Quaternion.Euler(0, 0, AngleDeg);
+        Debug.Log(Quater[2] + " " + Quater[3] + " " + (Quaternion.Euler(0, 0, AngleDeg)));
+        if (Quater[2] < 0 & Quater[3] < 0)
+        {
+            Debug.Log("buhg");
+        }
+
     }
 }
