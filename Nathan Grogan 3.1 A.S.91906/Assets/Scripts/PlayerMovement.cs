524db9c0c8b9b422aed5f0e77adfd25c6e087c5c
NathanGroganTBC <nathan.grogan@tbc.school.nz>
2023-05-19 09:24:34 +1200

    19/05/23

--- Nathan Grogan 3.1 A.S.91906/Assets/Scripts/PlayerMovement.cs
+++ Nathan Grogan 3.1 A.S.91906/Assets/Scripts/PlayerMovement.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor.Animations;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public Animator animator;
+
+
     string lookDirection = null;
+    int lookAxis = 2;
+    
+
     // Update is called once per frame
     void Update()
     {
@@ -17,16 +23,19 @@ public class PlayerMovement : MonoBehaviour
         Vector3 vertical = new Vector3(0.0f, Input.GetAxis("Vertical"), 0.0f);
         Vector3 mousePos = Input.mousePosition;
         Vector3 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
+        // This code makes the  "lookDirection" variable into a number, so up = 1 down = 2 left = 3 right = 4
         if (Input.GetAxis("Horizontal") < 0 | Input.GetAxis("Vertical") < 0)
         {
             if (Input.GetAxis("Horizontal") < 0)
             {
                 lookDirection = "Left";
+                lookAxis = 3;
+
             }
             if (Input.GetAxis("Vertical") < 0)
             {
                 lookDirection = "Down";
+                lookAxis = 2;
             }
         }
         else if (Input.GetAxis("Horizontal") > 0 | Input.GetAxis("Vertical") > 0)
@@ -34,10 +43,12 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetAxis("Horizontal") > 0)
             {
                 lookDirection = "Right";
+                lookAxis = 4;
             }
             if (Input.GetAxis("Vertical") > 0)
             {
                 lookDirection = "Up";
+                lookAxis = 1;
             }
         }
         
@@ -46,8 +57,16 @@ public class PlayerMovement : MonoBehaviour
         transform.position = transform.position + vertical * Time.deltaTime;
 
 
-        Debug.Log(lookDirection);
-
-
+        // Debug.Log(lookDirection + " " + Input.GetAxis("Horizontal") + " " + Input.GetAxis("Vertical"));
+        // checks if idle or not
+        if (Input.GetAxis("Horizontal") < 0.01 & Input.GetAxis("Horizontal") > -0.01 & Input.GetAxis("Vertical") < 0.01 & Input.GetAxis("Vertical") > -0.01)
+        {
+            animator.SetInteger("IsIdle", 1);
+        }
+        else
+        {
+            animator.SetInteger("IsIdle", 0);
+        }
+        animator.SetInteger("LookDirection", lookAxis);
     }
 }
